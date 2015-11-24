10c185e9c053c7de5398c5baf2300182221a157d
UTAGAWA Kiki <utagawakiki@gmail.com>
2015-11-25 00:42:26 +0900

    Get CALLBACK_URL from environment variables

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ CONSUMER_SECRET = environ.get('CONSUMER_SECRET')
 REQUEST_TOKEN_URL = 'https://www.hatena.com/oauth/initiate'
 AUTHORIZE_URL = 'https://www.hatena.ne.jp/oauth/authorize'
 AUTHORIZE_URL_SP = 'https://www.hatena.ne.jp/touch/oauth/authorize'
-CALLBACK_URL = 'http://localhost:5000/oauth/callback'
+CALLBACK_URL = environ.get('CALLBACK_URL')
 GET_ACCESS_TOKEN_URL = 'https://www.hatena.com/oauth/token'
 SCOPE = 'read_public,read_private,write_public,write_private'
 
